1aadaa7e0265026123f5205896c76c5ccbbadd54
hp <shafqatkb109@gmail.com>
2024-08-29 22:43:31 +0500

    hi hi hi hi

--- app/src/main/java/com/example/tiktokvideodownloader/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/example/tiktokvideodownloader/presentation/navigation/Navigation.kt
@@ -1,13 +1,13 @@
 package com.example.tiktokvideodownloader.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.core.net.toUri
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import com.example.tiktokvideodownloader.presentation.navigation.components.Screens.*
+import com.example.tiktokvideodownloader.presentation.navigation.components.Screens.TiktokScreen
+import com.example.tiktokvideodownloader.presentation.navigation.components.Screens.VideoViewScreen
 import com.example.tiktokvideodownloader.presentation.tiktok.TiktokScreen
 import com.example.tiktokvideodownloader.presentation.video_view.VideoPlayer
 
--- app/src/main/java/com/example/tiktokvideodownloader/util/Constants.kt
+++ app/src/main/java/com/example/tiktokvideodownloader/util/Constants.kt
@@ -6,7 +6,7 @@ import androidx.annotation.RequiresApi
 object Constants {
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     val permissionList =
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             listOf(
                 android.Manifest.permission.READ_MEDIA_VIDEO,
                 android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED,
